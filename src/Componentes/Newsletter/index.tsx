e7209ac7ec2d1d6714b3a525de1e865113951002
PeuStone <pedrohmotaaa@gmail.com>
2023-12-05 10:59:09 -0300

    Alterações do projeto

---
+++ src/Componentes/Newsletter/index.tsx
@@ -0,0 +1,24 @@
+import { AbCampoTexto } from 'alurabooksbase'
+import { useState } from 'react'
+import './Newsletter.css'
+
+const Newsletter = () => {
+    const [email, setEmail] = useState('')
+    return (<section className='Newsletter'>
+        <div>
+            <h6>Fique por dentro das novidades!</h6>
+            <p>Atualizações de e-books, novos livros, promoções e outros.</p>
+        </div>
+        <form>
+            <AbCampoTexto
+                darkmode={false}
+                value={email}
+                onChange={setEmail}
+                placeholder="Cadastre seu e-mail" 
+                label={''}
+                />
+        </form>
+    </section>)
+}
+
+export default Newsletter
