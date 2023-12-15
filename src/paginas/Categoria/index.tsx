9142c166ca4370a92c8f6e65093cdf5132073574
PeuStone <pedrohmotaaa@gmail.com>
2023-12-15 11:17:07 -0300

    Tornando o titulo principal da pagina dinamico

--- src/paginas/Categoria/index.tsx
+++ src/paginas/Categoria/index.tsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react"
 import TituloPrincipal from "../../Componentes/TituloPrincipal"
+import { ICategoria } from "../../interfaces/ICategoria"
+import http from "../../http"
+import { useParams } from "react-router-dom"
 
 const Categoria = () => {
+
+    const [categoria, setCategoria] = useState<ICategoria>()
+    const params = useParams()
+
+    useEffect(() => {
+        http.get('categorias', {
+            params: {
+                slug: params.slug
+            }
+        }).then(resposta => {
+            setCategoria(resposta.data[0])
+        })
+    }, [params.slug])
+
     return (
         <section>
-            <TituloPrincipal texto="Categoria" />
+            <TituloPrincipal texto={categoria?.nome ?? ''} />
         </section>
     )
 }
