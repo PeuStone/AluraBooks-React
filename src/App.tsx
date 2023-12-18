cfebbe7cf84b2f37837c6062b2503bb4795bcf9c
PeuStone <pedrohmotaaa@gmail.com>
2023-12-18 09:53:27 -0300

    Criando um Client Provider do React Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Rotas from './rotas';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <BrowserRouter>
-      <Rotas />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Rotas />
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 
