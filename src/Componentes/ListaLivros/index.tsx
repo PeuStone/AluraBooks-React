9e4604e1f671abd931473660081b93fcbeadf8c9
PeuStone <pedrohmotaaa@gmail.com>
2023-12-19 10:40:47 -0300

    Recebendo e exibindo os dados da API

--- src/Componentes/ListaLivros/index.tsx
+++ src/Componentes/ListaLivros/index.tsx
@@ -1,13 +1,20 @@
+import { useQuery } from "@tanstack/react-query"
 import { ICategoria } from "../../interfaces/ICategoria"
+import { obterProdutosDaCategoria } from "../../http"
+import CardLivro from "../CardLivro"
 
 interface ListaLivrosProps {
     categoria: ICategoria
 }
 
 const ListaLivros = ({ categoria }: ListaLivrosProps) => {
+
+    const { data: produtos } = useQuery({ queryKey: ['buscaLivrosCategoria', categoria], queryFn: () => obterProdutosDaCategoria(categoria) })
+
+
     return (
         <>
-        
+            {produtos?.map(livro => <CardLivro livro={livro} key={livro.id} />)}
         </>
     )
 }
