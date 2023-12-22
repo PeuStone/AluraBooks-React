b53bc04ca20c309f266d88fc180f862d2f64a9d9
PeuStone <pedrohmotaaa@gmail.com>
2023-12-22 11:01:05 -0300

    Criação do Provider do Apollo Client e implementação

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Rotas from './rotas';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import AbApolloClient from './Componentes/AbApolloClient';
 
 const queryClient = new QueryClient()
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Rotas />
-      </BrowserRouter>
-    </QueryClientProvider>
+    <AbApolloClient>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Rotas />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </AbApolloClient>
   )
 }
 
