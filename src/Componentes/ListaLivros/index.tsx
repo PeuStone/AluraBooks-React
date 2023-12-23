606c4f3078d758314359fbe33c3035ebe2423d0e
PeuStone <pedrohmotaaa@gmail.com>
2023-12-23 08:15:24 -0300

    Criando reativeVars

--- src/Componentes/ListaLivros/index.tsx
+++ src/Componentes/ListaLivros/index.tsx
@@ -4,6 +4,8 @@ import './ListaLivros.css'
 import { AbBotao, AbCampoTexto } from "alurabooksbase"
 import { useState } from "react"
 import { useLivros } from "../../graphql/livros/hooks"
+import { useReactiveVar } from "@apollo/client"
+import { livrosVar } from "../../graphql/livros/state"
 
 interface ListaLivrosProps {
   categoria: ICategoria
@@ -12,7 +14,13 @@ interface ListaLivrosProps {
 const ListaLivros = ({ categoria }: ListaLivrosProps) => {
 
   const [textoBusca, setTextoBusca] = useState('')
+  const livros = useReactiveVar(livrosVar)
+  console.log('livros =>', livros);
+  
   const { data, refetch } = useLivros(categoria)
+  if (data?.livros) {
+    livrosVar(data.livros)
+  }
   // const { data: produtos } = useQuery({ queryKey: ['buscaLivrosCategoria', categoria], queryFn: () => obterProdutosDaCategoria(categoria) })
 
   const buscarLivros = (event: React.FormEvent<HTMLFormElement>) => {
