31b36e66c5f1c17e38a458d2635aa396f3cef497
PeuStone <pedrohmotaaa@gmail.com>
2024-01-08 11:10:12 -0300

    Desenvolvimento e estilização da Pagina do Carrinho

---
+++ src/paginas/Carrinho/ItemCarrinho/index.tsx
@@ -0,0 +1,46 @@
+import { AbQuantidade } from "alurabooksbase"
+import { IItemCarrinho } from "../../../interfaces/IITemCarrinho"
+import { formatador } from "../../../utils/formatador-moeda"
+import lixeira from './assets/lixeira.png'
+import './ItemCarrinho.css'
+
+interface ItemCarrinhoProps {
+    item: IItemCarrinho
+}
+
+const ItemCarrinho = ({ item }: ItemCarrinhoProps) => {
+    return (
+        <div className="item-carrinho">
+            <figure>
+                <img src={item.livro.imagemCapa} alt={item.livro.descricao} />
+            </figure>
+            <div className="detalhes">
+                <ul className="lista-itens">
+                    <li className="titulo">{item.livro.titulo}</li>
+                    <li className="descricao">{item.livro.descricao}</li>
+                    <li className="autor">Por: {item.livro.autor.nome}</li>
+                </ul>
+            </div>
+            <div>
+                <ul className="preco">
+                    <li className="label">
+                        <strong>Preço:</strong>
+                    </li>
+                    <li className="valor">
+                        {formatador.format(item.opcaoCompra.preco)}
+                    </li>
+                </ul>
+            </div>
+            <div className="quantidade">
+                <AbQuantidade value={item.quantidade} onChange={() => console.log('[AbQuantidade] - onChange')} />
+            </div>
+            <div>
+                <button className="btn-excluir">
+                    <img src={lixeira} alt="Ícone de uma lixeira" />
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default ItemCarrinho
--- src/paginas/Carrinho/index.tsx
+++ src/paginas/Carrinho/index.tsx
@@ -1,11 +1,37 @@
+import { Link } from "react-router-dom"
+import TituloPrincipal from "../../Componentes/TituloPrincipal"
 import { useCarrinho } from "../../graphql/carrinho/hooks"
+import { formatador } from "../../utils/formatador-moeda"
+import { AbBotao } from "alurabooksbase"
+import ItemCarrinho from "./ItemCarrinho"
+import './Carrinho.css'
 
 const Carrinho = () => {
-    const carrinho = useCarrinho()
-    console.log(carrinho?.data);
+
+    const { data } = useCarrinho()
 
     return (
-        <h1>Carrinho</h1>
+        <section className="pagina-carrinho">
+            <TituloPrincipal texto="Minha Sacola" />
+            <div className="conteudo">
+                <h4>Itens selecionados</h4>
+                <div className="itens">
+                    {data?.carrinho?.itens.map((item, index) => <ItemCarrinho key={index} item={item} />)}
+                </div>
+                <div>
+                    <Link to='/'>Continuar comprando</Link>
+                </div>
+                <footer>
+                    <ul>
+                        <li>Total da compra</li>
+                        <li><strong>{formatador.format(data?.carrinho?.total || 0)}</strong></li>
+                        <li>
+                            <AbBotao texto="Finalizar Compra" />
+                        </li>
+                    </ul>
+                </footer>
+            </div>
+        </section>
     )
 }
 
